10946dad982d6d34385030ef125d7f5b51161b7e
sergushenkov <sergushenkov@gmail.com>
2024-05-14 11:07:00 +0300

    add list

--- fp-py-4.py
+++ fp-py-4.py
@@ -12,3 +12,4 @@ add10 = add(10)
 assert add10(20) == 30
 assert Just(20).then(add10) == Just(30)
 assert ListMonad(1, 2, 3).then(add10) == ListMonad(11, 12, 13)
+assert list(map(add10, [1, 2, 3])) == [11, 12, 13]

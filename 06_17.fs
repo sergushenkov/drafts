8a1c15c40652c4014459b6e0eff8483425efaadb
sergushenkov <sergushenkov@gmail.com>
2024-05-28 14:35:37 +0300

    fix

--- 06_17.fs
+++ 06_17.fs
@@ -1,7 +1,3 @@
-. Напишите функцию pow: string * int -> string, где pow(s,n) выдаёт строку s, повторенную n раз.
-. Напишите функцию-предикат isIthChar: string * int * char -> bool, где isIthChar(s,n,c) проверяет, равен ли n-й (начиная с нуля) символ строки s символу c.
-. Напишите функцию occFromIth: string * int * char -> int, где occFromIth(s,n,c) возвращает количество вхождений символа с в строке s, начиная с позиции n.
-
 // 17.1
 let rec pow = function
   | s, n when n = 0 -> ""

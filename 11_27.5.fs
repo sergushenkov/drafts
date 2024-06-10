198b8cf1e5c9ea7bccefb9b8db976e8ad601184b
sergushenkov <sergushenkov@gmail.com>
2024-06-10 16:11:42 +0300

    fix type f

--- 11_27.5.fs
+++ 11_27.5.fs
@@ -2,9 +2,9 @@ type F =
   | AM
   | PM
 
-type TimeOfDay = { hours: int; minutes: int; f: string }
+type TimeOfDay = { hours: int; minutes: int; f: F }
 
 let (.>.) x y = 
-  let a = if (x.f = "AM") then x.hours * 60 + x.minutes else 12 * 60 + x.hours * 60 + x.minutes
-  let b = if (y.f = "AM") then y.hours * 60 + y.minutes else 12 * 60 + y.hours * 60 + y.minutes
+  let a = if (x.f = AM) then x.hours * 60 + x.minutes else 12 * 60 + x.hours * 60 + x.minutes
+  let b = if (y.f = PM) then y.hours * 60 + y.minutes else 12 * 60 + y.hours * 60 + y.minutes
   a > b

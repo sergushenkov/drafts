cd27388cf9c8abd1826edded6225338d81d3e415
sergushenkov <sergushenkov@gmail.com>
2024-07-01 17:06:40 +0300

    fix list -> set

--- 16_42.3.fs
+++ 16_42.3.fs
@@ -1,4 +1,4 @@
 let rec allSubsets n k =
-    if k = 0 then [[]]
-    elif n = k then [List.init k ((+) 1)]
-    else (allSubsets (n - 1) k) @ List.map (fun subset -> n :: subset) (allSubsets (n - 1) (k - 1))
+    if k = 0 then Set.empty.Add(Set.empty)
+    elif n = k then Set.empty.Add(Set.ofList [1..n])
+    else Set.union (allSubsets (n - 1) k) (Set.map (fun subset -> Set.add n subset) (allSubsets (n - 1) (k - 1)))

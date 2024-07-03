2b1a5e9005d533b5436d458718be2759ae8fac50
sergushenkov <sergushenkov@gmail.com>
2024-07-03 15:36:06 +0300

    fix bigList

--- 19-48.4.fs
+++ 19-48.4.fs
@@ -1,9 +1,9 @@
 // 48.4.1
-let rec fibo1(n:int, n1:int, n0:int):int =
-    if n = 0 then n0
+let rec fibo1(n:int, n1:int, n2:int):int =
+    if n = 0 then n2
     elif n = 1 then n1
-    elif n = 2 then n1 + n0
-    else fibo1(n - 1, n0 + n1, n1)
+    elif n = 2 then n1 + n2
+    else fibo1(n - 1, n2 + n1, n1)
 
 // 48.4.2
 let fibo2(n:int, с):int =
@@ -15,6 +15,6 @@ let fibo2(n:int, с):int =
     fib_r n 0 1 id
 
 // 48.4.3
-let rec bigList(n, k) =
+let rec bigList n k =
   if n=0 then k []
   else bigList (n-1) (fun res -> k(1::res))

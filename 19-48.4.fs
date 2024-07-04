57f3cfeaea259b26906097f67a33f92750ad3bb4
sergushenkov <sergushenkov@gmail.com>
2024-07-04 16:19:17 +0300

    fix style

--- 19-48.4.fs
+++ 19-48.4.fs
@@ -1,19 +1,19 @@
 // 48.4.1
-let rec fibo1(n:int, n1:int, n2:int):int =
+let rec fibo1 n n1 n2 =
     if n = 0 then n2
     elif n = 1 then n1
     elif n = 2 then n1 + n2
-    else fibo1(n - 1, n2 + n1, n1)
+    else fibo1 (n - 1) (n2 + n1) n1
 
 // 48.4.2
-let rec fibo2(n:int, c):int =
+let rec fibo2 n c =
     match n with
     | 0 -> c 0
     | 1 -> c 1
-    | _ -> fibo2(n-1, fun x -> fibo2(n-2, fun y -> c (x + y)))
+    | _ -> fibo2 (n-1) (fun x -> fibo2 (n-2) (fun y -> c (x + y)))
 
 // 48.4.3
-let rec bigList (n:int, k) =
+let rec bigList n k =
     match n with
     | 0 -> k []
-    | _ -> 1 :: bigList (n - 1, k)
+    | _ -> 1 :: bigList (n - 1) k

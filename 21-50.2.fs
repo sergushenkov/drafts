7003bad1fc145af73da158515fc9cc2ad14aff4c
sergushenkov <sergushenkov@gmail.com>
2024-07-05 09:30:33 +0300

    fs 21-50.1

---
+++ 21-50.2.fs
@@ -0,0 +1,15 @@
+// 50.2.1
+let fac_seq =
+    let rec factorial n =
+        match n with
+        | 0 | 1 -> 1
+        | _ -> n * factorial (n-1)
+    seq { for i in 0 .. System.Int32.MaxValue do yield factorial i }
+
+// 50.2.2
+let seq_seq =
+    seq {
+        for i in 0 .. System.Int32.MaxValue do
+            yield i
+            yield -i
+    }

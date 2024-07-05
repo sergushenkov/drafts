5918cfca809bf538517ada1aca7680e4aad769f9
sergushenkov <sergushenkov@gmail.com>
2024-07-05 10:03:07 +0300

    fix first point fac_seq

--- 21-50.2.fs
+++ 21-50.2.fs
@@ -4,7 +4,7 @@ let fac_seq =
         match n with
         | 0 | 1 -> 1
         | _ -> n * factorial (n-1)
-    seq { for i in 1 .. System.Int32.MaxValue do yield factorial i }
+    seq { for i in 0 .. System.Int32.MaxValue do yield factorial i }
 
 // 50.2.2
 let seq_seq =
